b372e0dce68a8b51ce373cce8df66891529a9df0
Leon van der Noll <leonvdnoll@gmail.com>
2020-09-28 14:39:30 +0200

    fixed delete endpoint

--- card_maker_app/signals/card.py
+++ card_maker_app/signals/card.py
@@ -10,12 +10,12 @@ from card_maker_app.models import Card, User
 
 @receiver(post_delete, sender=Card)
 def card_post_delete(sender, **kwargs):
-    if kwargs['instance'].move_1:
-        kwargs['instance'].move_1.delete()
-    if kwargs['instance'].move_2:
-        kwargs['instance'].move_2.delete()
-    if kwargs['instance'].move_3:
-        kwargs['instance'].move_3.delete()
+    if kwargs['instance'].move1:
+        kwargs['instance'].move1.delete()
+    if kwargs['instance'].move2:
+        kwargs['instance'].move2.delete()
+    if kwargs['instance'].move3:
+        kwargs['instance'].move3.delete()
     if kwargs['instance'].ability:
         kwargs['instance'].ability.delete()
 
